0be87b310fc362940d76601c51658e33fa728518
Adam Saleh <adsaleh946@gmail.com>
2024-04-07 13:08:21 +1000

    Update FileHandling.java

--- Functions/FileHandling.java
+++ Functions/FileHandling.java
@@ -1,10 +1,7 @@
 package Functions;
 import Classes.*;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 // ---------------------------------------------------------------------------------------------------- //
@@ -19,46 +16,58 @@ public class FileHandling {
     }
 
     private void UserReader() throws Exception {
-        File file = new File("Data/users.csv");
-        Scanner reader = new Scanner(file);
-        reader.nextLine(); // HEADER
+        String normalizedPath = Paths.get("./Data/users.csv").normalize().toString();
+        File file = new File(normalizedPath);
 
-        while (reader.hasNextLine()) {
-            String[] user = reader.nextLine().split(",");
-            Staff staff = null;
+        if (file.exists()) { 
+            Scanner reader = new Scanner(file);
+            reader.nextLine(); // HEADER
 
-            if (user.length == 5) {
-                staff = new Technician(user[0], user[1], user[2], user[3], Integer.parseInt(user[4]));
-                Data.Get().AddTechy((Technician)staff);
+            while (reader.hasNextLine()) {
+                String[] user = reader.nextLine().split(",");
+                Staff staff = null;
+
+                if (user.length == 5) {
+                    staff = new Technician(user[0], user[1], user[2], user[3], Integer.parseInt(user[4]));
+                    Data.Get().AddTechy((Technician)staff);
+                }
+                else { staff = new Staff(user[0], user[1], user[2], user[3]); }
+
+                Data.Get().AddStaff(staff);       
             }
-            else { staff = new Staff(user[0], user[1], user[2], user[3]); }
 
-            Data.Get().AddStaff(staff);       
-        }
-
-        reader.close();
+            reader.close();
+        } 
     }
 
     private void TicketReader() throws Exception {
         Pattern LINE_PATTERN = Pattern.compile(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+        String normalizedPath = Paths.get("./Data/tickets.csv").normalize().toString();
+        File file = new File(normalizedPath);
 
-        File file = new File("Data/tickets.csv");
-        Scanner reader = new Scanner(file);
-        reader.nextLine(); // HEADER
+        if (file.exists()) {
+            Scanner reader = new Scanner(file);
+            reader.nextLine(); // HEADER
 
-        while (reader.hasNextLine()) {
-            String[] ticket = LINE_PATTERN.split(reader.nextLine(), -1);
-            Data.Get().AddTicket(new Ticket(ticket[0].trim(), ticket[1].trim(), ticket[2].trim(), ticket[3].trim(), ticket[4].trim(), ticket[5].trim()));
+            while (reader.hasNextLine()) {
+                String[] ticket = LINE_PATTERN.split(reader.nextLine(), -1);
+                Data.Get().AddTicket(new Ticket(ticket[0].trim(), ticket[1].trim(), ticket[2].trim(), ticket[3].trim(), ticket[4].trim(), ticket[5].trim()));
+            }
+
+            reader.close();
         }
-
-        reader.close();
     }
     // ---------------------------------------------------------------------------------------------------- //
     public static void NewStaff(Staff newStaff) throws IOException {
-        FileWriter file = new FileWriter(new File("Data/users.csv"), true);
-        PrintWriter writer = new PrintWriter(file);
-        writer.println(newStaff.toString());
-        writer.close();
+        String normalizedPath = Paths.get("./Data/users.csv").normalize().toString();
+        File file = new File(normalizedPath);
+
+        if (file.exists()) {
+            FileWriter fWriter = new FileWriter(file, true);
+            PrintWriter writer = new PrintWriter(fWriter);
+            writer.println(newStaff.toString());
+            writer.close();
+        }
     }
     // ---------------------------------------------------------------------------------------------------- //
 }
