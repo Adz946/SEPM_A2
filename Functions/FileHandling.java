566f29ad47b56e169f2939116485114af96694f7
Adam Saleh <adsaleh946@gmail.com>
2024-04-07 14:40:59 +1000

    Hard Coded Data :(

--- Functions/FileHandling.java
+++ Functions/FileHandling.java
@@ -16,7 +16,7 @@ public class FileHandling {
     }
 
     private void UserReader() throws Exception {
-        String normalizedPath = Paths.get("./Data/users.csv").normalize().toString();
+        String normalizedPath = Paths.get("./Data_/users.csv").normalize().toString();
         File file = new File(normalizedPath);
 
         if (file.exists()) { 
@@ -38,11 +38,24 @@ public class FileHandling {
 
             reader.close();
         } 
+        else {
+            // Techies Hard-coded
+            Data.Get().AddTechy(new Technician("Harry Styles", "h.styles@company.com", "0411 222 333", "01234_xX_StyleS_Xx_56789", 1));
+            Data.Get().AddTechy(new Technician("Niall Horan", "n.horan@company.com", "0422 333 444", "0_Xx_xX__HORAN__Xx_xX_0", 1));
+            Data.Get().AddTechy(new Technician("Liam Payne", "l.payne@company.com", "0433 444 555", "XxXx_xXxX__PaYnE__XxXx_xXxX", 1));
+            Data.Get().AddTechy(new Technician("Louis Tomlinson", "l.tomlinson@company.com", "0444 555 666", "21!_Jumbo_Junior_!12", 2));
+            Data.Get().AddTechy(new Technician("Zayn Malik", "z.malik@company.com", "0455 666 777", "_Malik-Al-Igbeer_45?", 2));
+
+            // Staff Hard-coded
+            Data.Get().AddStaff(new Staff("Sam Syne", "s.syne@company.com", "0466 777 888", "01234ABCDE56789fghij"));
+            Data.Get().AddStaff(new Staff("Juliet Jameson", "j.jameson@company.com", "0477 888 999", "abcde01234FGHIJ56789"));
+            Data.Get().AddStaff(new Staff("Jacob Heru", "j.heru@company.com", "0488 999 000", "SecretPassword_No0neCanGuess_THIS"));
+        }
     }
 
     private void TicketReader() throws Exception {
         Pattern LINE_PATTERN = Pattern.compile(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-        String normalizedPath = Paths.get("./Data/tickets.csv").normalize().toString();
+        String normalizedPath = Paths.get("./Data_/tickets.csv").normalize().toString();
         File file = new File(normalizedPath);
 
         if (file.exists()) {
@@ -56,10 +69,28 @@ public class FileHandling {
 
             reader.close();
         }
+        else {
+            // Tickets Hard-coded
+            Data.Get().AddTicket(new Ticket("T-100", "s.syne@company.com", "h.styles@company.com", 
+            "\"Hey, I'm trying to use the new Editing Software but am running into some issues with the updated features, how do I use those?\"", 
+            "LOW", "OPEN"));
+
+            Data.Get().AddTicket(new Ticket("T-105", "s.syne@company.com", "z.malik@company.com", 
+            "\"I've lost access to the 'Project T' Team's shared resources. I need to get major changes done by tonight!\"", 
+            "HIGH", "ARCHIVED"));
+
+            Data.Get().AddTicket(new Ticket("T-110", "j.jameson@company.com", "l.payne@company.com", 
+            "\"The Cloud is slow today, took me 15 minutes to load my dashboard.\"", 
+            "MEDIUM", "RESOLVED"));
+
+            Data.Get().AddTicket(new Ticket("T-115", "j.jameson@company.com", "l.tomlinson@company.com", 
+            "\"I've lost it! All my data!! I need a back-up recovery of 'Project L' Version 13.42!\"", 
+            "HIGH", "OPEN"));
+        }
     }
     // ---------------------------------------------------------------------------------------------------- //
     public static void NewStaff(Staff newStaff) throws IOException {
-        String normalizedPath = Paths.get("./Data/users.csv").normalize().toString();
+        String normalizedPath = Paths.get("./Data_/users.csv").normalize().toString();
         File file = new File(normalizedPath);
 
         if (file.exists()) {
@@ -68,6 +99,7 @@ public class FileHandling {
             writer.println(newStaff.toString());
             writer.close();
         }
+        else { System.out.println(".../Changes have been made!"); }
     }
     // ---------------------------------------------------------------------------------------------------- //
 }
