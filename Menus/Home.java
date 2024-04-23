cd1a4af9f57bd40c689e056075f5aa1dc0111731
Noah Etherington <95476019+noahj97@users.noreply.github.com>
2024-04-23 13:12:42 +1000

    Update Home.java
    Adds ticket report to the home menu, also added is the verification to only allow this feature to technicians

--- Menus/Home.java
+++ Menus/Home.java
@@ -1,20 +1,25 @@
 package Menus;
 import Functions.*;
+import Classes.Staff;
+import Classes.Technician;
 
 public class Home {
-    public int Menu() {
+    public int Menu() {  
+        Staff currentStaff = Data.Get().GetActiveStaff(); 
         System.out.printf("---------- %s ---------- \n" +
            "[1] View Tickets \n" +
            "[2] Open Ticket \n" +
-           "[3] Log Out \n" +
-           "[4] Exit \n", Data.Get().GetActiveStaff().GetName());
+           (currentStaff instanceof Technician ? "[3] Generate Report \n" : "") +
+           "[4] Log Out \n" +
+           "[5] Exit \n", Data.Get().GetActiveStaff().GetName());
         System.out.print(" >> ");
         String input = InputReader.Get().nextLine();
 
         if (input.equals("1")) { return 4; }
         else if (input.equals("2")) { return 5; }
-        else if (input.equals("3")) { return 0; }
-        else if (input.equals("4")) { App.ExitProgram(); }
+        else if (input.equals("3") && currentStaff instanceof Technician) { return 6; }
+        else if (input.equals("4")) { return 0; }
+        else if (input.equals("5")) { App.ExitProgram(); }
         else App.WriteError("Only Select Between the Available Options");
 
         return 3;
